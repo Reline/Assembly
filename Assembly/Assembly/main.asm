99c24051aadc120ecafde38a756d1bc24af5f306
Reline <nathanielreline@gmail.com>
2016-02-18 16:43:09 -0700

    Random int print to screen

--- Assembly/Assembly/main.asm
+++ Assembly/Assembly/main.asm
@@ -1,30 +1,37 @@
-INCLUDE Irvine32.inc ; included for DumpRegs use
-
-.data ; variable initializations
-	; all intrinsic data types ; the values associated have no real meaning
-	; unsigned can hold a larger pos val, and no neg val
-	; signed can hold pos and neg val
-	
-	wordz WORD 20 ; 16-bit unsigned integer
-	swordz SWORD -20 ; 16-bit signed integer
-	
+INCLUDE Irvine32.inc
 
 .code ; indicates where code begins
 
 main proc ; define our process name designated in the linker
-	; 32-bit environment, so 32-bit registers and data sizes are the max
-	mov ax, wordz ; put 20 in ax
-	call DumpRegs
+	
+	call Randomize ; sets random seed
+	mov eax,1000 ; determines our range (0-1000)
 
-	mov bx, swordz ; put -20 in bx
-	sub ax, bx ; 20 - (-20) = 40
-	call DumpRegs
+	call RandomRange ; choose a random number from our range
+	call WriteDec ; write our random number to the console
 
-	sub ax, 40 ; 40 - 40 = 0
-	call DumpRegs
+	; write newline
+	mov eax, 0Ah
+	call WriteChar
 
-	ret ;ret gives back the value in the last register used (eax in this case)
+	call RandomRange ; choose a random number from our range
+	call WriteDec ; write our random number to the console
 
-	;call ExitProcess ; exits process. we want to see the dumpregs, so don't call this
-main ENDP ; end our main process
+	; write newline
+	mov eax, 0Ah
+	call WriteChar
+
+	call RandomRange ; choose a random number from our range
+	call WriteDec ; write our random number to the console
+
+	; write newline
+	mov eax, 0Ah
+	call WriteChar
+
+	call RandomRange ; choose a random number from our range
+	call WriteDec ; write our random number to the console
+
+	ret
+
+	main ENDP ; end our main process
 END
