62e79c2dcbd546700ac0677cad29cf53037af58f
Reline <nathanielreline@gmail.com>
2016-03-25 18:13:49 -0700

    Str_find

--- Assembly/Assembly/main.asm
+++ Assembly/Assembly/main.asm
@@ -1,117 +1,84 @@
 INCLUDE Irvine32.inc
 
 .data
-count = 65000 ; 'roof' for prime number search
-prompt BYTE "Prime numbers from 0-", 0
-done BYTE "Done", 0
-
-.data?
-array DWORD count DUP(?) ; declare array in uninitialized data segment
+source BYTE "ABC",0
+target BYTE "123ABC342432",0
+pos DWORD ?
 
 .code
-main proc
+;--------------------------------------------------------- 
+; Str_find 
+; 
+; Searches for first matching occurence of a source string 
+; inside a target string and returns the matching position.
+; Receives: EAX (source string) and EBX (target string)
+; Returns: found: EAX = index, ZF = 1
+;		   not found: EAX = undefined, ZF = 0
+;---------------------------------------------------------
 	
-	; zero out array
-	mov esi, OFFSET array
-	mov ecx, count
-L1: mov eax, 0
-	mov [esi], eax
-	add esi, TYPE array
-	loop L1
-
-	; move back to beginning of array
-	mov esi, OFFSET array ; 1
-	add esi, TYPE array ; 2
-	add esi, TYPE array ; 3
-	mov ecx, 3 ; store current index in ecx ; 1st index = 1st element
-	mov ebx, 2 ; store current prime number
-
-L2: ; skip if current index equals current prime number
-	cmp ecx, ebx
-	je con
-	; DIV stores the quotient in AX and remainder in DX
-	mov eax, ecx
-	xor edx, edx
-	div bx
-	; if the remainder is 0, set current index to composite
-	cmp dx, 0
-	jnz con
+Str_find proc uses esi edi
+	xor ecx, ecx
+	mov edx, LENGTHOF source
+	dec edx ; don't count null terminator
+L1: 
 	mov eax, [esi]
-	mov eax, 1
-	mov [esi], eax
-
-	; go to next index if we haven't finished searching with current prime number
-con: add esi, TYPE array
-	inc ecx
-	cmp ecx, count
-	jle L2
-
-	; set esi back to the correct index
-	mov esi, OFFSET array
-	mov ecx, 1
-L3: add esi, TYPE array
-	inc ecx
-	cmp cx, bx
-	jne L3
+	mov ebx, [edi]
+	; inc src if we find a char that matches
+	cmp al, bl
+	jnz L2
+	; if equal
+	inc esi ; increment source
+	inc edi ; increment target
+	inc ecx ; increment current index of target
+	dec edx ; decrement number of characters left to compare in source
+	; end if found whole string
+	cmp edx, 0
+	jz success
+	jmp L1
 	
-	; find the next prime number
-L4: add esi, TYPE array
-	inc ecx
-	; check if we reached the end of the array and found no new prime numbers
-	cmp ecx, count
-	jg L5 ; quit if we have gone past the array
-	; if value at current index = 0, set new prime number
-	mov eax, [esi]
-	mov eax, 0
-	cmp [esi], eax
-	jnz L4
-	mov ebx, ecx
-	jmp L2
+L2: ; if not equal
+	inc edi ; increment target
+	inc ecx ; increment current index of target
+	mov edx, LENGTHOF source ; reset characters to compare in source
+	dec edx ; don't count null terminator
+	; if target is too small to contain source, end
+	mov eax, LENGTHOF target
+	dec eax ; don't count null terminator
+	sub eax, ecx
+	sub eax, edx
+	cmp eax, 0
+	jge L1
+	jmp fail
 
-	
-L5:	; print entire array
-	;mov esi, OFFSET array
-	;mov ecx, 1
-;L7: mov eax, ecx
-	;call WriteDec
-	;call Crlf
-	;mov eax, [esi]
-	;call WriteDec
-	;call Crlf
-	;call Crlf
-	;inc ecx
-	;add esi, TYPE array
-	;cmp ecx, count
-	;jle L7
-
-	; print all prime numbers
-	mov edx, OFFSET prompt
-	call WriteString
-	mov eax, count
-	call WriteDec
-	call Crlf
-	call Crlf
-	mov esi, OFFSET array
-	; 1 is not a prime number, so skip it
-	mov ecx, 2
-	add esi, TYPE array
-L6: mov eax, ecx
-	mov eax, 0
-	cmp [esi], eax
-	jnz composite
-	mov eax, ecx
-	call WriteDec
-	call Crlf
-composite:	add esi, TYPE array
-	inc ecx
-	cmp ecx, count
-	jle L6
-	
-	mov edx, OFFSET done
-	call Crlf
-	call WriteString
-
+success:
+	mov ah, 1000000b ; set zero flag to 1
+	sahf ; copy into flags register
+	mov eax, ecx ; set eax to index where string was found
+	sub eax, LENGTHOF source
+	inc eax ; don't count null terminator
 	ret
-main ENDP
 
-END main
+fail:
+	mov ah, 0b ; set zero flag to 0
+	sahf ; copy into flags register
+	mov eax, 0 ; set eax to undefined (0)
+	ret
+
+Str_find endp
+
+main proc
+	; move pointers to registers
+	mov esi, OFFSET source
+	mov edi, OFFSET target
+	call Str_find
+	jnz notFound
+	mov pos, eax
+
+notFound:
+	nop
+
+	call WriteDec
+	ret
+main ENDP ; end main process
+
+END main ; end main.asm
