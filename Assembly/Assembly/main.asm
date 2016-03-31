c624902c0e4bc73801f6abfab501bfb258fdc1d6
Reline <nathanielreline@gmail.com>
2016-03-31 00:44:04 -0700

    Copy/convert to lowercase

--- Assembly/Assembly/main.asm
+++ Assembly/Assembly/main.asm
@@ -1,28 +1,20 @@
 INCLUDE Irvine32.inc
 
-.code
-POINT STRUCT
-   X WORD ?
-   Y WORD ?
-POINT ENDS
-
-Rectangle STRUCT
-   upperLeft POINT <>
-   lowerRight POINT <>
-Rectangle ENDS
-
 .data
-; create an array of three identical rectangles
-myRectArray Rectangle 3 DUP(<<10,20>,<50,60>>)
+arrayA BYTE "ABCDEFGHI",0
+arrayB BYTE LENGTHOF arrayA
 
 .code
 main proc
-   mov esi, OFFSET myRectArray ; index 1
-   add esi, SIZEOF Rectangle ; increment to index 2
-   add esi, SIZEOF Rectangle ; increment to index 3
-   mov eax, 80 ; ready 80 to move
-   ; place 80 inside the current Rectangle.upperLeft.X
-   mov (Rectangle PTR [esi]).upperLeft.X, ax 
+   mov ecx, LENGTHOF arrayA
+   mov esi, OFFSET arrayA
+   mov edi, OFFSET arrayB
+L1:
+   LODSB
+   OR al, 00100000b
+   call WriteChar
+   STOSB
+   loop L1
 
    ret
 main ENDP ; end main process
